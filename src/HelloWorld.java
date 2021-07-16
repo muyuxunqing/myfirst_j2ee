9eb7b85bcb2763a093b905f27bb9759db25a2a95
muyuxunqing <48301886+muyuxunqing@users.noreply.github.com>
2021-07-16 11:56:39 +0800

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+        Integer.parseInt();
+        Integer a = 2;
+        int b = a + 1;
+
+    }
+}
