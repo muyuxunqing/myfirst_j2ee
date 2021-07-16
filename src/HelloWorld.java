fb135924dd9a212e376450f2b503a4748c3e5450
muyuxunqing <48301886+muyuxunqing@users.noreply.github.com>
2021-07-16 17:07:21 +0800

    push commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,7 @@
 public class HelloWorld {
     public static void main(String args[]){
         System.out.println("Hello git");
-        Integer a = 2;
-        int b = a + 1;
         System.out.println("hot-fix change");
-
+        System.out.println("push test");
     }
 }
